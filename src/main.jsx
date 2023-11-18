3ab3e40104753396951b1eac14b9f38efad57a6d
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-11-17 23:23:30 -0600

    Provides redux store to the app

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App.jsx';
 import './index.css';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>
 );
