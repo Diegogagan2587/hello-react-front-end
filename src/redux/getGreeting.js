2c296f5c2026d340f8769b15a67f2987611fb892
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-11-17 23:22:13 -0600

    Create AsyncThunk Request: getGreetings.js

--- src/redux/getGreeting.js
+++ src/redux/getGreeting.js
@@ -2,10 +2,10 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 
 const API_GREETING_URL = 'http://localhost:3000/greetings';
 
-const getConcerts = createAsyncThunk('getConcerts', async () => {
+const getGreeting = createAsyncThunk('getGreeting', async () => {
   const response = await fetch(API_GREETING_URL);
   const data = await response.json();
   return data;
 });
 
-export default getConcerts;
+export default getGreeting;
